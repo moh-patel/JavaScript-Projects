dfdb60a5584f7f47167500908dd7c8f43f6a7671
moh <mohpatel6@gmail.com>
2020-08-28 18:19:40 +0100

    finished First Project

--- project1/app.js
+++ project1/app.js
@@ -1,1 +1,10 @@
-console.log('Hello');
+let colors = ['red', 'blue', 'green', 'violet'];
+
+document.getElementById('change').addEventListener("click", changeColor);
+
+
+function changeColor(){
+    let i = Math.floor(Math.random() * colors.length);
+    document.body.style.backgroundColor = colors[i];
+}
+document.getElementById('change').style.backgroundColor = 'grey';
