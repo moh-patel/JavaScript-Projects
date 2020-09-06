62627c1376f4b64751e937853a73a466988225a0
moh <mohpatel6@gmail.com>
2020-09-06 12:57:32 +0100

    project6 image-slider

---
+++ project6 image_slider/app.js
@@ -0,0 +1,20 @@
+(function(){
+    let container = document.querySelector('#container');
+    let index = 0;
+    let images = ['car-0', 'car-1', 'car-2', 'car-3', 'car-4'];
+    document.querySelectorAll('button').forEach(button => {
+        button.addEventListener('click', function(){
+            if(button.id == 'next'){
+                index = (index + 1) % 5;
+            }else{
+                index = index-1;
+                if(index == -1){
+                    index = 4;
+                }
+            }
+            container.style.backgroundImage = 'url("images/' + images[index] + '.jpeg")';
+            
+        });
+    });
+    
+})();
